64110c850e320f3774eb7136db9bab1f07b35758
jasonpizzaboy <80494361+jasonpizzaboy@users.noreply.github.com>
2022-03-11 23:16:31 +0900

    sumBtwAandB

---
+++ day1/04.sumBtwAandB.js
@@ -0,0 +1,7 @@
+//https://programmers.co.kr/learn/courses/30/lessons/12912
+
+function sumAWithB(num1, num2) {
+  console.log(((num1 + num2) * (Math.abs(num1 - num2) + 1)) / 2);
+}
+
+sumAWithB(1, 5);
