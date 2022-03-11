2f8a037ea030091f1db4d65aab894668b3d27cb1
jasonpizzaboy <80494361+jasonpizzaboy@users.noreply.github.com>
2022-03-11 23:57:28 +0900

    blockPhoneNum

---
+++ day1/09.blockPhoneNum.js
@@ -0,0 +1,11 @@
+//https://programmers.co.kr/learn/courses/30/lessons/12948
+
+function blockPhoneNum(str) {
+  let star = "*".repeat(str.length - 4);
+  let restNum = str.slice(-4);
+  let totalNum = star + restNum;
+
+  console.log(totalNum);
+}
+
+blockPhoneNum("0123456789");
