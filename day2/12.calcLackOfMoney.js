80859fbf92cabfaaac10f07ea19b1925596304e9
jasonpizzaboy <80494361+jasonpizzaboy@users.noreply.github.com>
2022-03-12 07:44:54 +0900

    calcLackOfMoney

---
+++ day2/12.calcLackOfMoney.js
@@ -0,0 +1,13 @@
+//https://programmers.co.kr/learn/courses/30/lessons/82612
+
+function calc(price, money, count) {
+  let lack = 0;
+  let totalPrice = 0;
+  for (let i = 1; i <= count; i++) {
+    totalPrice += price * i;
+  }
+  lack = Math.abs(totalPrice - money);
+  totalPrice > money ? console.log(lack) : console.log(0);
+}
+
+calc(3, 20, 4);
