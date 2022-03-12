062769726f5d8cc909fc7c5b98ba527171195cf6
jasonpizzaboy <80494361+jasonpizzaboy@users.noreply.github.com>
2022-03-12 21:12:16 +0900

    Update 09.blockPhoneNum.js

--- day1/09.blockPhoneNum.js
+++ day1/09.blockPhoneNum.js
@@ -11,3 +11,14 @@ function blockPhoneNum(str) {
 blockPhoneNum("0123456789");
 
 //slice() : https://im-developer.tistory.com/103
+
+// //풀이2
+// function solution(num) {
+//   let answer = "";
+//   for (let i = 0; i < num.length; i++) {
+//     i < num.length - 4 ? (answer += "*") : (answer += num[i]);
+//   }
+//   console.log(answer);
+// }
+
+// solution("0123456789");
