59bae08c7c589f7a1b391a350af3c9bc5fbea5b0
jasonpizzaboy <80494361+jasonpizzaboy@users.noreply.github.com>
2022-03-14 09:35:25 +0900

    13.2016

--- day2/13.2016.js
+++ day2/13.2016.js
@@ -8,6 +8,7 @@ function returnDayName(m, d) {
 
   for (let i = 1; i < m; i++) {
     totalDayCnt += monCnt[i - 1];
+    console.log(totalDayCnt);
   }
   totalDayCnt += d - 1;
   answer = dayName[totalDayCnt % 7];
@@ -17,13 +18,13 @@ function returnDayName(m, d) {
 
 returnDayName(5, 24);
 
-//풀이2
-function solution(a, b) {
-  const monthDay = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-  const weekDay = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"];
+// //풀이2
+// function solution(a, b) {
+//   const monthDay = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+//   const weekDay = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"];
 
-  let days = b;
-  for (let i = 0; i < a - 1; i++) days += monthDay[i];
+//   let days = b;
+//   for (let i = 0; i < a - 1; i++) days += monthDay[i];
 
-  return weekDay[days % 7];
-}
+//   return weekDay[days % 7];
+// }
---
+++ day3/13.2016.js
@@ -0,0 +1,15 @@
+function getDayNm(m, d) {
+  let day = ["FRI", "SAT", "SUN", "NOM", "TUE", "WED", "THU"];
+  let mon = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  let dayNum = 0;
+
+  for (let i = 1; i < mon[m]; i++) {
+    //31번 반복 -> dayNum 값이 undefined, 배열의 값으로 입력이 아닌 index로 사용!
+    dayNum += mon[i - 1];
+    console.log(dayNum);
+  }
+
+  console.log(day[((dayNum + d) % 7) - 1]);
+}
+
+getDayNm(5, 24);
