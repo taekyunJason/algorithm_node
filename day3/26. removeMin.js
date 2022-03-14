50eb3e67520266039ec46325e9cdb7d784e669a6
jasonpizzaboy <80494361+taekyunJason@users.noreply.github.com>
2022-03-15 01:47:42 +0900

    Update 26. removeMin.js

--- day3/26. removeMin.js
+++ day3/26. removeMin.js
@@ -1,14 +1,15 @@
 //https://programmers.co.kr/learn/courses/30/lessons/12935
 
 function solution(arr) {
-  // arr.pop(Math.min(arr))
   arr.splice(arr.indexOf(Math.min(...arr)), 1);
-  //     arr.length===0?arr.push(-1):arr;
+  arr.sort((a, b) => b - a);
   if (arr.length < 1) {
     return [-1];
   }
   return arr;
 }
+
+solution([3, 1, 2]);
 
 // function solution1(arr) {
 //   arr.pop(Math.min(arr));
