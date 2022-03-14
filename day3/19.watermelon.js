38eb832e4a3554d2e5648c7b8d1c9db59555df4d
jasonpizzaboy <80494361+jasonpizzaboy@users.noreply.github.com>
2022-03-14 20:19:29 +0900

    create 30.unfinished

--- day3/19.watermelon.js
+++ day3/19.watermelon.js
@@ -1,3 +1,12 @@
 function waterMelon(num) {
   return "수박".repeat(num).slice(0, num);
 }
+
+//풀이2
+function solution(n) {
+  let result = "";
+  for (let i = 0; i < n; i++) {
+    i % 2 == 0 ? (result += "박") : (result += "수");
+  }
+  return result;
+}
---
+++ day3/20.unfinished.js
@@ -0,0 +1,10 @@
+function unfinishedPlayer(participant, completion) {
+  participant.sort();
+  completion.sort();
+
+  for (let i = 0; i < participant.length; i++) {
+    if (participant[i] !== completion[i]) {
+      return participant[i];
+    }
+  }
+}
