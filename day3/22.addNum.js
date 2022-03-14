5dc12a8047ae59e9c0270b778dac2ccdac1f4d2b
jasonpizzaboy <80494361+jasonpizzaboy@users.noreply.github.com>
2022-03-14 15:48:12 +0900

    Create 22.addNum.js

---
+++ day3/22.addNum.js
@@ -0,0 +1,32 @@
+function addNum(num) {
+  let answer = 0;
+  while (num >= 10) {
+    answer += num % 10;
+    num /= 10;
+  }
+  console.log(answer);
+}
+
+solution1(123);
+
+//풀이2
+function solution(num) {
+  let answer = 0;
+  let word = String(num);
+
+  for (let i = 0; i < word.length; i++) {
+    answer += parseInt(word[i]);
+  }
+  console.log(answer);
+}
+
+//풀이3
+function solution1(n) {
+  var a = (n + "").split("");
+  var b = 0;
+  for (var i = 0; i < a.length; i++) {
+    b += parseInt(a[i]);
+  }
+  console.log(b);
+  //return n.toString().split('').reduce((a, b) => (a * 1) + (b * 1));
+}
