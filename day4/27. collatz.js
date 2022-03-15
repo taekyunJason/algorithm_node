233a9c1aa95d1c29977c0c803d52ffff2f4d1304
jasonpizzaboy <80494361+taekyunJason@users.noreply.github.com>
2022-03-15 10:02:47 +0900

    Update 27. collatz.js

--- day4/27. collatz.js
+++ day4/27. collatz.js
@@ -1,1 +1,32 @@
-function collatz() {}
+//https://programmers.co.kr/learn/courses/30/lessons/12943
+//필요과정
+//(숫자 짝홀 구분, /2, *3+1) cnt 확인, num==1 체크
+
+function solution(num) {
+  if (num == 1) {
+    return 0;
+  }
+  let cnt = 0;
+  for (let i = 0; i < 500; i++) {
+    num % 2 == 0 ? (num /= 2) : (num = num * 3 + 1);
+    cnt++;
+
+    if (num === 1) {
+      return cnt;
+    }
+  }
+  if (num !== 1) {
+    return -1;
+  }
+}
+solution(6);
+
+//풀이2
+function collatz(num) {
+  var answer = 0;
+  while (num != 1 && answer != 500) {
+    num % 2 == 0 ? (num = num / 2) : (num = num * 3 + 1);
+    answer++;
+  }
+  return num == 1 ? answer : -1;
+}
