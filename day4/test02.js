71c57af7776f4fd4d641a509ea0387ce89606b9a
jasonpizzaboy <80494361+taekyunJason@users.noreply.github.com>
2022-03-16 12:41:14 +0900

    Update test02.js

--- day4/test02.js
+++ day4/test02.js
@@ -26,3 +26,41 @@ function solution(month, day) {
   return `"${final_day}"`;
 }
 console.log(solution(1, 18));
+
+//풀이3
+function solution(month, day) {
+  let answer = 0;
+
+  let arr = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  for (let i = month - 1; i <= arr.length; i++) {
+    answer += arr[i];
+    if (answer >= day + 98) {
+      month = i + 1;
+      day = arr[i] - (answer - (day + 98));
+      return `${month}월 ${day}일`;
+    }
+    if (i == 11) {
+      i = 0;
+    }
+  }
+}
+
+console.log(solution(1, 18));
+
+//풀이4
+function solution(month, day) {
+  let result = "";
+  let days = 0;
+  date = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  month -= 1;
+  while (days + date[month] < 98) {
+    days += date[month++];
+    month > 11 ? (month = 0) : {};
+  }
+  day += 98 - days;
+  if (date[month] < day) day -= date[month++];
+  result = month + 1 + "월 " + day + "일";
+  return result;
+}
+console.log(solution(1, 18));
