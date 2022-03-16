2df882981a99e3d20cddc7c04ab1d35133f70a25
jasonpizzaboy <80494361+taekyunJason@users.noreply.github.com>
2022-03-16 22:23:20 +0900

    Update 30. minSqare.js

--- day5/30. minSqare.js
+++ day5/30. minSqare.js
@@ -2,42 +2,40 @@
 //[[60,50],[30,70],[60,30],[80,40]] 각 원소의 첫번째 요소의 max1, 각 요소의 두번째 요소의 max2//
 //if(max1>=max2 && max2의 첫번째 값<= 각 요소의 두번째 요소의 두번째로 큰값) => size = max1*각 요소의 두번째 요소의 두번째로 큰값
 
-// function minSqare(sizeArr) {
-//   let maxHor = 0;
-//   let maxVer = 0;
-//   let maxVerSec = 0;
-//   let size = 0;
+function minSqare(sizeArr) {
+  let maxHor = 0;
+  let maxVer = 0;
+  let maxVerSec = 0;
+  let size = 0;
 
-//   for (let i = 0; i < sizeArr.length; i++) {
-//     maxHor = Math.max(sizeArr[i][0]);
-//     maxVer = Math.max(sizeArr[i][1]);
-//     console.log(maxHor, maxVer);
-//   }
-// }
-
-// console.log(
-//   minSqare([
-//     [60, 50],
-//     [30, 70],
-//     [60, 30],
-//     [80, 40],
-//   ])
-// );
-
-//풀이2
-function solution(sizes) {
-  sizes.map((item) => item.sort((a, b) => b - a));
-  console.log(sizes);
-  let hMax = Math.max.apply(
-    null,
-    sizes.map((item) => item[0])
-  );
-  let vMax = Math.max.apply(
-    null,
-    sizes.map((item) => item[1])
-  );
-  return hMax * vMax;
+  console.log(sizeArr);
+  for (let i = 0; i < sizeArr.length; i++) {}
+  sizeArr.sort((a, b) => b - a);
 }
+
+console.log(
+  minSqare([
+    [60, 50],
+    [30, 70],
+    [60, 30],
+    [80, 40],
+  ])
+);
+
+// //풀이2
+// function solution(sizes) {
+//   sizes.map((item) => item.sort((a, b) => b - a));
+//   console.log(sizes);
+//   let hMax = Math.max.apply(
+//     null,
+//     sizes.map((item) => item[0])
+//   );
+//   let vMax = Math.max.apply(
+//     null,
+//     sizes.map((item) => item[1])
+//   );
+//   return hMax * vMax;
+// }
 
 // //풀이3
 // function solution(sizes) {
@@ -45,11 +43,11 @@ function solution(sizes) {
 //     return Math.max(...newSizes.map(e => e[0])) * Math.max(...newSizes.map(e => e[1]));
 // }
 
-console.log(
-  solution([
-    [60, 50],
-    [30, 70],
-    [60, 30],
-    [80, 40],
-  ])
-);
+// console.log(
+//   solution([
+//     [60, 50],
+//     [30, 70],
+//     [60, 30],
+//     [80, 40],
+//   ])
+// );
