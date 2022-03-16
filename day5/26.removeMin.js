b01353d4eebcf9578277f1719bc3a51cc8271af0
jasonpizzaboy <80494361+taekyunJason@users.noreply.github.com>
2022-03-16 18:36:15 +0900

    Update 26.removeMin.js

--- day5/26.removeMin.js
+++ day5/26.removeMin.js
@@ -1,18 +1,29 @@
 //https://programmers.co.kr/learn/courses/30/lessons/12935
 //배열에서 min값 구하기 - 배열에서 min 값 제거하여 리턴/ 빈배열은 -1리턴
 
-function removeMin(arr) {
-  let min = 0;
-  let answer = [];
+// function removeMin(arr) {
+//   let min = 0;
+//   let answer = [];
 
-  min = Math.min(...arr); //Math.min값 가져올때는 ()에 배열의 원소를 보내야 함!
-  console.log(min);
-  //배열에서 min값 제거
-  answer = arr.filter((num) => num > min);
-  console.log(answer);
-  return answer.length == 0 ? answer.push(-1) : answer;
-}
-
+//   arr.sort((a, b) => b - a);
+//   if (arr.length === 1) {
+//     answer.push(-1);
+//     return answer;
+//   }
+//   min = Math.min(...arr);
+//   answer = arr.filter((num) => num > min);
+//   return answer;
+// }
+// console.log(removeMin([10]));
+//Math.min값 가져올때는 ()에 배열의 원소를 보내야 함!
+//배열에서 min값 제거
 //특정값 삭제 방법
 
-console.log(removeMin([4, 3, 2, 1]));
+//풀이
+function solution(arr) {
+  arr.splice(arr.indexOf(Math.min(...arr)), 1);
+  if (arr.length < 1) return [-1];
+  return arr;
+}
+
+console.log(solution([10]));
