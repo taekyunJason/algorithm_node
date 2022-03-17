3e109eb28a5be813c6c3c63df98875bb1187fc2f
jasonpizzaboy <80494361+taekyunJason@users.noreply.github.com>
2022-03-17 15:56:51 +0900

    Update 13.2016.js

--- day3/13.2016.js
+++ day3/13.2016.js
@@ -1,14 +1,18 @@
 function getDayNm(m, d) {
-  let day = ["FRI", "SAT", "SUN", "NOM", "TUE", "WED", "THU"];
+  let day = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"];
   let mon = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
   let dayNum = 0;
 
-  for (let i = 1; i < mon[m]; i++) {
+  for (let i = 1; i < m; i++) {
     //31번 반복 -> dayNum 값이 undefined, 배열의 값으로 입력이 아닌 index로 사용!
     dayNum += mon[i - 1];
+    console.log(dayNum);
   }
-
+  dayNum -= mon[m - 1];
+  console.log(dayNum);
   return day[((dayNum + d) % 7) - 1];
 }
 
-console.log(getDayNm(5, 24));
+console.log(getDayNm(1, 11));
+
+//수정필요
