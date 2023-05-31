955d65cfdd3d95146a43ff4c21c05859ddf20a93
jason taekyun Kim <80494361+taekyunJason@users.noreply.github.com>
2023-06-01 08:36:22 +0900

    Update 프로세스.js

--- Programmers/Lv.2/프로세스.js
+++ Programmers/Lv.2/프로세스.js
@@ -33,14 +33,14 @@ function solution(priorities, location) {
   //priorities 배열이 비어있을 때까지 반복
   while (priorities.length != 0) {
     max_value = Math.max(...priorities)
-    console.log('this is maxValue=>', max_value)
+    // console.log('this is maxValue=>', max_value)
 
     // priorities 배열의 0번 인덱스 값이 최대값보다 작은 경우 - 큐의 맨뒤로 보내기
     if (priorities[0] < max_value) {
       // priorities와 arr배열의 첫번째 요소를 제거해서 맨뒤에 추가 (0번째 인덱스 값 맨 뒤로 순서 바꾸기)
       priorities.push(priorities.shift())
       arr.push(arr.shift())
-      console.log(arr)
+      //   console.log(arr)
     }
     // priorities 배열의 0번 인덱스 값이 최대값 이상인 경우
     else {
@@ -49,7 +49,7 @@ function solution(priorities, location) {
       // priorities 첫번째 요소 제거
       priorities.shift()
       // arr 첫번째 요소를 제거한 것이 위치와 같으면 answer 리턴
-      console.log('this is arr=>', arr.shift(), answer)
+      //   console.log('this is arr=>', arr.shift(), answer)
       if (arr.shift() == location) return answer
     }
   }
